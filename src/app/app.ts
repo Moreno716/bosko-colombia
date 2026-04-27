76527bb74122adca2b90f3282820f50506977e0f
Moreno716 <juandiegomc00@gmail.com>
2026-04-27 16:20:56 -0500

    feat: initialize project with responsive navigation, hero section, guide carousel, and tour showcase components

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,6 +10,30 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   isScrolled = signal(false);
+  currentGuideIndex = signal(0);
+  guideImages = [
+    'guia1.jfif',
+    'guia2.jpeg'
+  ];
+
+  nextGuideImage() {
+    this.currentGuideIndex.update(i => (i + 1) % this.guideImages.length);
+  }
+
+  prevGuideImage() {
+    this.currentGuideIndex.update(i => (i - 1 + this.guideImages.length) % this.guideImages.length);
+  }
+
+  setGuideImage(index: number) {
+    this.currentGuideIndex.set(index);
+  }
+
+  constructor() {
+    setInterval(() => {
+      this.nextGuideImage();
+    }, 5000);
+  }
+
 
   tours = [
     {
@@ -39,7 +63,31 @@ export class App {
     this.isScrolled.set(window.scrollY > 50);
   }
 
+  reviews = [
+    {
+      name: 'Ainhoa Ramonell Vidal',
+      stars: 5,
+      text: 'Una de las mejores experiencias durante nuestro viaje a Colombia. En nuestro caso hicimos La Carbonera con Jeffersson y ha sido una maravilla. Es súper cercano y amable. Sin duda muy recomendable 😀'
+    },
+    {
+      name: 'Patricia Fernández Rivas',
+      stars: 5,
+      text: 'El día de hoy con Jeferson ha sido muy guay 😊 si quieres disfrutar de una zona totalmente mágica en Colombia en el eje cafetero, sentirte arropada y que vives su diversidad desde bien dentro, te recomiendo compartir tu tiempo y tu experiencia con él!!'
+    },
+    {
+      name: 'Laura Martinez',
+      stars: 5,
+      text: 'Tuvimos una experiencia increíble en el Valle del Cocora gracias a Jeferson. Es un guía muy profesional, amable y con un gran conocimiento del lugar, su historia y la naturaleza que lo rodea. Siempre estuvo atento al grupo, explicó todo con claridad y transmitió mucho amor por su región. Además, hizo el recorrido muy ameno y seguro. Sin duda, lo recomendamos al 100% para conocer y disfrutar el Cocora como se debe.'
+    },
+    {
+      name: 'Raúl Blanco',
+      stars: 5,
+      text: 'La experiencia con Jefferson ha sido algo fuera de lo normal. Empezando por la misma Reserva de la carbonera, un lugar mágico que con la experiencia, carisma y conocimiento de Jefferson lo han hecho aún más especial. Cuando la buena gente además es experta hace que todo merezca la pena. Gracias una y mil veces.'
+    }
+  ];
+
   scrollTo(id: string) {
+
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
